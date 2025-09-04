6e045077476e90767f69218d04bba415186a86ca
4dominic888 <4dominin888@gmail.com>
2025-09-04 16:35:45 -0500

    feat: DonaYa Logo

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -3,4 +3,7 @@ class DonaYaConstants {
 
   static const String appName = 'DonaYa';
   static const String appLogo = 'https://avatars.githubusercontent.com/u/92335293?v=4&size=64'; //TODO: Change this
+
+  static const String logoSvgLight = 'assets/svg/donaya-light.svg';
+  static const String logoSvgDark = 'assets/svg/donaya-dark.svg';
 }
